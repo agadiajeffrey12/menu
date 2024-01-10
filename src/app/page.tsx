0088d5a791ab6021518af27aa0ecd0c2f7983e12
Jeffery Agadia <agadiajeffrey@gmail.com>
2024-01-10 22:22:41 +0100

    json to object

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,37 @@
 "use client";
 import Image from "next/image";
-import Data from "../utils/Data.json";
+import { Data } from "@/utils/Data";
 import Showcase from "./components/Showcase";
 import { useEffect, useState } from "react";
 import First from "./components/First";
 
+
+interface item{
+  softDrinks:{
+    name:string,
+    price:string,
+    category:string | undefined,
+    image:string|undefined
+  },
+  Whiskey:{
+    name:string,
+    price:string,
+    category:string | undefined,
+    image:string|undefined
+  },
+  Cream:{
+    name:string,
+    price:string,
+    category:string | undefined,
+    image:string|undefined
+  },
+}
 export default function Home() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     const arr: any = [];
-    Object.keys(Data).forEach((key) => {
+    Object.keys(Data).forEach((key:any) => {
       const value = Data[key];
       arr.push({ name: key, value });
     });
@@ -25,35 +46,9 @@ export default function Home() {
       <header className="flex w-[98.5%] h-[4rem] border-b items-center mb-4">
         <h2 className="text-3xl font-bold">Menu</h2>
       </header>
-      {/* <div className="flex flex-wrap gap-2">
-            <Showcase name={Data.SoftDrinks[0].category?.toString()} item={Data.SoftDrinks}/>
-            <Showcase name={Data.Whiskey[0].category?.toString()} item={Data.Whiskey}/>
-            <div className="flex flex-col gap-2">
-              <Showcase name={Data.Tequila[0].category?.toString()} item={Data.Tequila}/>
-              <Showcase name={Data.Gin[0].category?.toString()} item={Data.Gin}/>
-            </div>
-            <Showcase name={Data.Rose[0].category?.toString()} item={Data.Rose}/>
-            <Showcase name={Data.Alc[0].category?.toString()} item={Data.Alc}/>
-            <Showcase name={Data.NonAlc[0].category?.toString()} item={Data.NonAlc}/>
-            <Showcase name={Data.Rice[0].category?.toString()} item={Data.Rice}/>
-            <Showcase name={Data.SHOTS[0].category?.toString()} item={Data.SHOTS}/>
-            <Showcase name={Data.ED[0].category?.toString()} item={Data.ED}/>
-            <Showcase name={Data.Cream[0].category?.toString()} item={Data.Cream}/>
-            <Showcase name={Data.Protein[0].category?.toString()} item={Data.Protein}/>
-            <Showcase name={Data.Bitters[0].category?.toString()} item={Data.Bitters}/>
-            <Showcase name={Data.Water[0].category?.toString()} item={Data.Water}/>
-            <Showcase name={Data.Soup[0].category?.toString()} item={Data.Soup}/>
-            <Showcase name={Data.Bb[0].category?.toString()} item={Data.Bb}/>
-            <Showcase name={Data.Extras[0].category?.toString()} item={Data.Extras}/>
-            <Showcase name={Data.Juice[0].category?.toString()} item={Data.Juice}/>
-            <Showcase name={Data.Brandy[0].category?.toString()} item={Data.Brandy}/>
-
- */}
-
-      {/* </div> */}
       <div className="w-full flex flex-wrap gap-2">
         {data?.map((item: any, i: number) => (
-          <First key={i} image={item.value[0].image} text={item.name} />
+          <First key={i} image={item.value[0].image} text={item.value[0].category} />
         ))}
       </div>
     </main>
