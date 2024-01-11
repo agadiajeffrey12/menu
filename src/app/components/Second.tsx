ec903c4e80ef3f3f1110f89e7d56404eecfe3d0e
Jeffery Agadia <agadiajeffrey@gmail.com>
2024-01-11 15:30:58 +0100

    text

--- src/app/components/Second.tsx
+++ src/app/components/Second.tsx
@@ -6,8 +6,8 @@ export default function Second({item}:any) {
     <div  className='w-[48%] max-w-[15rem] h-[13rem]  rounded bg-[#294d61] overflow-hidden relative'>
       <img src={item.image } className='w-full h-full object-cover'/>
       <div className="absolute bottom-0 left-0 w-full h-[50%] flex justify-between bg-slate-700/50 p-2">
-        <p className='laptop:text-xl phone:text-[1rem]'>{item.name}</p>
-        <p className='laptop:text-xl phone:text-[1rem]'>{item?.price}</p>
+        <p className='laptop:text-xl phone:text-[.8rem]'>{item.name}</p>
+        <p className='laptop:text-xl phone:text-[.8rem]'>{item?.price}</p>
       </div>
     </div>
   )
