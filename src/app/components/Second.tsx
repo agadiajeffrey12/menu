f4c0283445e46e5846dbdac2cec7ddf5788e863e
Jeffery Agadia <agadiajeffrey@gmail.com>
2024-01-15 13:12:43 +0100

    change

--- src/app/components/Second.tsx
+++ src/app/components/Second.tsx
@@ -4,10 +4,10 @@ export default function Second({item}:any) {
   // console.log(item)
   return (
     <div  className='w-[48%] max-w-[15rem] h-[13rem]  rounded bg-[#294d61] overflow-hidden relative'>
-      <img src={item.image } className='w-full h-full object-cover'/>
-      <div className="absolute bottom-0 left-0 w-full h-[50%] flex justify-between bg-slate-700/50 p-2">
+      <img src={item.img } className='w-full h-full object-cover'/>
+      <div className="absolute bottom-0 left-0 w-full h-[50%] flex flex-col  justify-between bg-slate-700/50 p-2">
         <p className='laptop:text-xl phone:text-[.8rem]'>{item.name}</p>
-        <p className='laptop:text-xl phone:text-[.8rem]'>{item?.price}</p>
+        <p className='laptop:text-xl phone:text-[1rem] font-semibold text-slate-800'>{item?.price}</p>
       </div>
     </div>
   )
